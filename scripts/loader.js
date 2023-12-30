e835e9e936500f4fc3e7a34df1309723a830636c
Adedero <118962395+Adedero@users.noreply.github.com>
2023-12-30 08:58:23 +0100

    overlay changes

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,8 +1,13 @@
 export function showOverlay(element) {
-    window.addEventListener('DOMContentLoaded', function() {
-        const overlay = document.getElementById(element);
-        overlay.style.display = 'flex';
-    });
+    window.addEventListener('beforeunload', function() {
+        const overlay = document.getElementById(element)
+        overlay.style.display = 'flex'
+    }); 
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const overlay = document.getElementById(element)
+        overlay.style.display = 'flex'
+    })
 }
 
 export function hideOverlay(element) {
